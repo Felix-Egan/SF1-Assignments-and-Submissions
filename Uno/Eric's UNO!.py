56e4a78e3c10c9669f4bac9df64cfd20ceb6317e
GravitexYT <148166523+Felix-Egan@users.noreply.github.com>
2024-10-20 20:43:07 -0400

    i want to sleep.....
    Changed File name
    Implemented last fixes
    Hopefully this version works//////

--- Uno/Eric's UNO!.py
+++ Uno/Eric's UNO!.py
@@ -1,15 +1,15 @@
-#region: Imports ✔
+# Imports 
 import random
 import os
 import time
 cls = lambda: os.system('cls' if os.name == 'nt' else 'clear')
-#endregion
 
-#region: Pregame constants (Note that the number of cards has been adjusted to the actual game's card count = 108) ✔
+# Pregame constants (Note that the number of cards has been adjusted to the actual game's card count = 108) 
 face_up_pile = []
 colors = ["Red", "Yellow", "Blue", "Green"]
 digits = ["+2", "+2", "Reverse", "Reverse", "Skip", "Skip"]
 digits.extend(list(range(10)) + list(range(1, 10)))
+digits = [str(digit) for digit in digits]
 draw_pile = [(digit, color) for digit in digits for color in colors] # Renamed deck to draw_pile for convenience and readibility
 wild_cards = [("Wild Color Change", "Black"), ("Wild Draw Four", "Black")]
 draw_pile.extend(wild_cards * 4)
@@ -19,25 +19,27 @@ players_called_UNO = []
 penalizable_players = []
 cls()
 print("""
-As Eric said many times, we were tasked with making a simplified version of UNO. Now, to which I gladly responded with "Respectfully, no." so I made a full-fledged version of UNO instead! (I have not slept in 3 days 👍)
+As Eric said many times, we were tasked with making a simplified version of UNO, to which I gladly responded with "Respectfully, no." so I made a full-fledged version of UNO instead! (I have not slept in 3 days 👍)
 This version works in a very similar fashion to the normal UNO game we all love (until someone starts with 4 wild cards and we all want to do is scare them while they are drinking a glass of juice so it splashes all over
-their clothes... Yeah, I see you Antonin!!). This version only DOESN'T include the following features that UNO players sometimes implement, mainly: NO Stacking +2s and +4s, NO Reversing a +2 or +4 (otherwise known as the
-ULTIMATE UNO REVERSE!!!), and finally +2s and +4s do NOT skip turns in this version. I don't know why that's even a considered rule to begin with! Anyways, have fun! Max playercount of *15* btw, otherwise things break :/
+their clothes... Yeah, I see you Antonin!!!). This version only DOESN'T include the following features that UNO players sometimes implement, mainly: NO Stacking +2s and +4s, NO Reversing a +2 or +4 (otherwise known as the
+ULTIMATE UNO REVERSE!!!), and finally +2s and +4s do NOT skip turns in this version. I don't even know why that's a considered rule to begin with! Anyways, have fun! Max playercount of *15* btw, otherwise things break :/
 """)
-input("Press Enter to continue... \n")
+input("""
+PS: I've done my very best to squeeze out any potential bugs and hiccups that might arise. If ever a bug does show up, please leave a comment in the code somewhere and send it back
+to me so I can look back with veteran eyes at the countless hours of sleep I missed out on to make this script.
+Press Enter to continue...""")
 num_players = int(input("Enter number of players: ")) 
 cls()
-player_hands = [[draw_pile.pop(0)  for i in range(7)] for i in range(num_players)]
-#endregion
+player_hands = [[draw_pile.pop(0) for i in range(7)] for i in range(num_players)]
 
-#region:Special Cards ✔
+# Special Cards 
 def reverse_card():
     global reverse_play # Sorry for using global variables Eric. They just make my life so much easier for this script ¯\(ツ)/¯ Plus its just a boolean... It shouldn't break anything
     reverse_play = not reverse_play
     print("The order of play is now reversed!")
 
 def skip_card(current_player):
-    print(f"Player {current_player + 2} skips their turn")
+    print(f"Player{(current_player - 1) % num_players if reverse_play else (current_player + 1) % num_players} skips their turn")
     current_player = ((current_player - 1) % num_players) if reverse_play else ((current_player + 1) % num_players)
     return current_player
 
@@ -54,22 +56,22 @@ def draw_four_color_change_card(current_player, draw_pile, face_up):
     face_up = change_color(face_up)
     opponent_draw_card(player_hands, current_player, draw_pile, 4)
     return face_up
-#endregion
 
-# Game Start ✔
+# Game Start 
 def start_game():
     face_up = draw_pile.pop(0)
+    face_up_pile.append(face_up)
     if face_up[1] == "Black":
         face_up_pile.append(face_up)
         start_game()
     current_player = 0
     main_loop(player_hands, face_up, draw_pile, face_up_pile, num_players, current_player)
 
-# Play Validation ✔
+# Play Validation 
 def valid_play(card, face_up):
     return card[1] == face_up[1] or card[0] == face_up[0] or card[1] == "Black"
 
-# Refresh draw_pile ✔
+# Refresh draw_pile 
 def refresh_draw_pile(face_up_pile, draw_pile):
     if not draw_pile:
         random.shuffle(face_up_pile)
@@ -77,27 +79,26 @@ def refresh_draw_pile(face_up_pile, draw_pile):
         face_up_pile = []
         print("All cards in the Face Up pile have been reshuffled into the draw_pile.")
 
-# Color changing ✔
+# Color changing 
 def change_color(face_up):
     print("Choose the new color (enter index):")
-    for color in colors: print(f"{colors.index(color) + 1}: {color}")
+    for color in colors: 
+        print(f"{colors.index(color) + 1}: {color}")
     chosen_index = int(input("Card index: ")) - 1
-    
-    if colors[chosen_index] == face_up_pile[1]:
-        print("New color cannot be the same as previous color. Please choose another color...")
-        change_color()
-    else: 
-        face_up = (face_up[0], colors[chosen_index])
-        return face_up
+    print(f"The new color is {colors[chosen_index]}")
+    face_up = (face_up[0], colors[chosen_index])
+    return face_up
 
-# Call Uno ✔
+# Call Uno 
 def play_call_UNO(player_hand, face_up, draw_pile, current_player):
-    face_up = play_round(player_hand, face_up, draw_pile, current_player)
+    round_results = play_round(player_hands[current_player], face_up, draw_pile, current_player)
+    face_up = round_results[0]
+    current_player = round_results[1]
     players_called_UNO.append(f"Player{current_player+1}")
     print(f"Player {current_player + 1} called UNO!")
     return face_up
 
-# Draw a card ✔
+# Draw a card 
 def draw_card(player_hand, face_up, draw_pile, current_player):
     if draw_pile:
         print("You draw a card.")
@@ -113,12 +114,12 @@ def draw_card(player_hand, face_up, draw_pile, current_player):
             print(f"And you have no valid cards in your hand: {player_hand}")
             print("You skip your turn.")
 
-# Round Play ✔
+# Round Play 
 def play_round(player_hand, face_up, draw_pile, current_player):
     valid_cards = [card for card in player_hand if valid_play(card, face_up)]
     if not valid_cards:
         print(f"You have no valid cards in your hand: {player_hand}")
-        draw_card(player_hand, draw_pile, face_up, current_player)
+        draw_card(player_hand, face_up, draw_pile, current_player)
     else:
         print("Choose a card to play (enter index):")
         for card in player_hand:
@@ -128,6 +129,7 @@ def play_round(player_hand, face_up, draw_pile, current_player):
         if valid_play(chosen_card, face_up):
             player_hand.pop(chosen_index)
             face_up = chosen_card
+            face_up_pile.append(face_up)
             print(f"You play the {chosen_card}")
             if   chosen_card[0] == "Wild Color Change": face_up = change_color(face_up)
             elif chosen_card[0] == "Wild Draw Four":  face_up = draw_four_color_change_card(current_player, draw_pile, face_up)
@@ -136,8 +138,8 @@ def play_round(player_hand, face_up, draw_pile, current_player):
             elif chosen_card[0] == "+2": opponent_draw_card(player_hands, current_player, draw_pile, 2)
         else:
             print("Invalid card. Start again.")
-            face_up = play_round(player_hand, face_up, draw_pile, current_player)
-    return face_up
+            play_round(player_hand, face_up, draw_pile, current_player)
+    return [face_up, current_player]
 
 def manage_UNO_players(player_hands):
     for player in players_called_UNO:
@@ -146,7 +148,6 @@ def manage_UNO_players(player_hands):
 # Main Game Loop
 def main_loop(player_hands, face_up, draw_pile, face_up_pile, num_players, current_player):
     while all(player_hands):
-        face_up_pile.append(face_up)
         refresh_draw_pile(face_up_pile, draw_pile)
         manage_UNO_players(player_hands)
         player_card_count = [f"Player{i+1}: {len(player_hands[i])} [{'UNO!' if f'Player{i+1}' in players_called_UNO else ''}]" for i in range(num_players)]
@@ -159,17 +160,42 @@ def main_loop(player_hands, face_up, draw_pile, face_up_pile, num_players, curre
         else:
             print("Please enter an index...")
             main_loop(player_hands, face_up, draw_pile, face_up_pile, num_players, current_player)
-        if 0 > play <3: # ooh look a heart ❤️
+        if 0 > play <3: 
             print("Please enter a valid index...")
             main_loop(player_hands, face_up, draw_pile, face_up_pile, num_players, current_player)
         
-        if   play == 0: draw_card(player_hands[current_player], face_up, draw_pile, current_player)
-        elif play == 1: face_up = play_round(player_hands[current_player], face_up, draw_pile, current_player)
+        if play == 0: 
+            if not draw_pile:
+                print("The draw pile is empty.")
+                valid_cards = [card for card in player_hand if valid_play(card, face_up)]
+                if valid_cards:
+                    print(f" But you have valid cards in your hand: {valid_cards}")
+                    print("You must play these cards to continue the game.")
+                    round_results = play_round(player_hands[current_player], face_up, draw_pile, current_player)
+                    face_up = round_results[0]
+                    current_player = round_results[1]
+                else: 
+                    print(f"And you have no valid cards in your hand: {player_hand}")
+                    print("You skip your turn.")
+            else:
+                draw_card(player_hands[current_player], face_up, draw_pile, current_player)
+        
+        
+        elif play == 1: 
+            round_results = play_round(player_hands[current_player], face_up, draw_pile, current_player)
+            face_up = round_results[0]
+            current_player = round_results[1]
+       
+       
         elif play == 2: 
-            if len(player_hands[current_player]) == 2 and [card for card in player_hands[current_player] if valid_play(card, face_up)]: face_up = play_call_UNO(player_hands[current_player], face_up, draw_pile)
+            if len(player_hands[current_player]) == 2 and [card for card in player_hands[current_player] if valid_play(card, face_up)]: 
+                face_up = play_call_UNO(player_hands[current_player], face_up, draw_pile)[0]
+                current_player = play_call_UNO(player_hands[current_player], face_up, draw_pile)[1]
             else:
                 print("You are unable to call UNO! just yet. Please enter a valid index...")
                 main_loop(player_hands, face_up, draw_pile, face_up_pile, num_players, current_player)
+        
+        
         elif play == 3: 
             for player_hand in player_hands: 
                 if len(player_hand) == 1 and f"Player{player_hands.index(player_hand) + 1}" not in players_called_UNO: penalizable_players.append(f"Player{player_hands.index(player_hand) + 1}")
@@ -189,9 +215,8 @@ def main_loop(player_hands, face_up, draw_pile, face_up_pile, num_players, curre
             main_loop(player_hands, face_up, draw_pile, face_up_pile, num_players, current_player)
 
         current_player = ((current_player - 1) % num_players) if reverse_play else ((current_player + 1) % num_players)
-        time.sleep(3)
+        time.sleep(1)
         cls()
-
     for player_hand in player_hands:
         if not player_hand:
             exit(f"Player {player_hands.index(player_hand) + 1} has no more cards in their hand. They win!")
