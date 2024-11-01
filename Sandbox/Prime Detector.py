01b034c22ff7f8f5e138f6937fa54a4327d96bc1
Felix Egan <felix.egan@icloud.com>
2024-11-01 08:26:06 -0400

    change output - PrimeDetector

--- Sandbox/Prime Detector.py
+++ Sandbox/Prime Detector.py
@@ -1,13 +1,13 @@
 n  = int(input("Prime Check: "))
 def is_prime(n):
     if n <= 1 or n % 2 == 0 or n % 3 == 0 or str(n)[-1] in "024568":
-        return False
+        return f"False: Negative, Even, Divisible by 2 or 3, or ends in 0, 2, 4, 6, 8"
     if n <= 3:
-        return True
+        return "True"
     i = 5
     while i * i <= n:
         if n % i == 0 or n % (i + 2) == 0:
             return f"False: divisible by {i} or {i + 2}"
         i += 6
-    return True
+    return "True"
 print("Is Prime? ", is_prime(n))
