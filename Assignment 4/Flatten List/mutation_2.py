5783f8905033c46b56761451cea691bfdf5163ad
GravitexYT <148166523+Felix-Egan@users.noreply.github.com>
2024-12-01 13:37:51 -0500

    Finished Mutation for Flatten-List

--- Assignment 4/Flatten List/mutation_2.py
+++ Assignment 4/Flatten List/mutation_2.py
@@ -1,11 +1,11 @@
 def flatten_list(lst):
     '''
-    Changed the append method to extend method in the else block. 
+    Changed the type comparison from type(i) == lst to type(i) == list rendering the if condition always false.
     '''
     result = []
     for i in lst:
-        if type(i) == list:
+        if type(i) == lst:
             result.extend(flatten_list(i))
         else:
-            result.extend(i)
+            result.append(i)
     return result
